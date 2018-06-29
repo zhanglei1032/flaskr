ccd3deb21e864105b169ab46934e43cc1c0f336c
zhanglei1032 <zhanglei1032@gmail.com>
2018-06-29 17:22:20 +0800

    更改启动中遇到的错误

--- auth.py
+++ auth.py
@@ -3,7 +3,7 @@ import functools
 from flask import (
 	Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
-from werkzeug.sercurity import check_password_hash, generate_password_hash
+from werkzeug.security import check_password_hash,generate_password_hash
 
 from flaskr.db import get_db
 
